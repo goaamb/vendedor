8897220bcb7acfb7608910780b2de4f73272bdcd
Alvaro Justo Michel Barrera <goaamb@gmail.com>
2013-01-12 12:26:33 -0400

    - correccion usuario perfil

--- apps/views/usuario/cabecera-perfil.php
+++ apps/views/usuario/cabecera-perfil.php
@@ -16,18 +16,10 @@ if (isset ( $seccion ) && isset ( $usuario ) && $usuario) {
 			?><script type="text/javascript">
 			usuarioID="<?=$usuario->id;?>";
 			</script><?php
-			break;
+		break;
 		case "articulo" :
-			$c = $this->articulo->cantidadOfertas ( $articulo->id, $usuario->id );
-			$nofertas = $this->articulo->cantidadOfertas ( $articulo->id );
-			$nofertas = $nofertas ? $nofertas->cantidad : 0;
-			$ofertante = $c && $c->cantidad > 0;
-			$comprador = $usuario->id === $articulo->comprador;
-			if ($transaccion) {
-				$comprador = $usuario->id === $transaccion->comprador->id;
-			}
-			$ver = isset ( $articulo ) && ($usuario->id == $articulo->usuario->id || $ofertante || $comprador || ($this->myuser && $articulo->terminado == 1));
-			break;
+			$ver = true;
+		break;
 	}
 	if ($ver) {
 		?><div class="user-data clearfix">
@@ -42,11 +34,7 @@ if (isset ( $seccion ) && isset ( $usuario ) && $usuario) {
 		if ($usuario->estado == "Baneado") {
 			print "class='baneado'";
 		}
-		?>><?=ucfirst($usuario->seudonimo)?></span> <a
-						href="home/modal/votos/votos/<?=$usuario->id?>" class="nmodal"><span
-						class="green">+<?=$usuario->positivo?></span>
-					<?php if($usuario->negativo>0){?><span class="red">-<?=$usuario->negativo?></span><?php }?></a>
-					<span class="localization"><?=($usuario->pais?$usuario->pais->nombre:"")?></span>
+		?>><?=ucfirst($usuario->seudonimo)?></span> <span class="localization"><?=($usuario->pais?$usuario->pais->nombre:"")?></span>
 				</h1>
 			<?php if($seccion=="perfil" && $this->myuser && $this->myuser->id == $usuario->id && $this->myuser->estado!=="Baneado"){?>
 			<p class="actions">
@@ -63,26 +51,6 @@ if (isset ( $seccion ) && isset ( $usuario ) && $usuario) {
 								array (
 										"url" => "store/" . $usuario->seudonimo,
 										"seccion" => "Tienda" 
-								),
-								array (
-										"url" => "store/" . $usuario->seudonimo . "/sell",
-										"seccion" => "Ventas" . ($totalVentas > 0 ? " ($totalVentas)" : "") 
-								),
-								array (
-										"url" => "store/" . $usuario->seudonimo . "/self",
-										"seccion" => "Compras" . ($totalCompras > 0 ? " ($totalCompras)" : "") 
-								),
-								array (
-										"url" => "store/" . $usuario->seudonimo . "/following",
-										"seccion" => "Seguimientos" . ($totalSeguimientos > 0 ? " ($totalSeguimientos)" : "") 
-								),
-								array (
-										"url" => "store/" . $usuario->seudonimo . "/messages",
-										"seccion" => "Mensajes" . ($totalMensajes > 0 ? " ($totalMensajes)" : "") 
-								),
-								array (
-										"url" => "store/" . $usuario->seudonimo . "/billing",
-										"seccion" => "Cuentas" . ($totalCuentas > 0 ? " ($totalCuentas)" : "") 
 								) 
 						);
 					} else {
@@ -92,28 +60,11 @@ if (isset ( $seccion ) && isset ( $usuario ) && $usuario) {
 					if ($usuario->estado == "Baneado") {
 						$baneado = true;
 					} else {
-						$link1 = "home/modal/enviar-mensaje-privado/mensaje/$usuario->id";
-						$link2 = "home/modal/denunciar/usuario/$usuario->id";
 						$class = "nmodal";
-						if ($usuarioExterno && ! $this->myuser) {
-							$link1 = "login/" . str_replace ( "=", "", base64_encode ( uri_string () ) );
-							$link2 = $link1;
-							$class = "";
-						}
 						$listaSecciones = array (
 								array (
 										"url" => "store/" . $usuario->seudonimo,
 										"seccion" => "Tienda" 
-								),
-								array (
-										"url" => $link1,
-										"seccion" => "enviar mensaje privado",
-										"class" => $class 
-								),
-								array (
-										"url" => $link2,
-										"seccion" => "denunciar",
-										"class" => $class 
 								) 
 						);
 					}
@@ -164,288 +115,10 @@ if (isset ( $seccion ) && isset ( $usuario ) && $usuario) {
 			</div>
 		<?php
 				}
-				break;
+			break;
 			
 			case "articulo" :
-				if (! $articulo->terminado) {
-					if (! $ofertante) {
-						if ($nofertas > 0) {
-							if ($articulo->tipo == "Fijo") {
-								$tituloCabecera = "Tiene " . $this->articulo->cantidadOfertasPendientes ( $articulo->id ) . " ofertas pendientes: <a class='nmodal' href='articulo/modal/ofertas/ofertas/$articulo->id'>ver ofertas</a>";
-							} else {
-								$tituloCabecera = "Tu artículo se vendera.";
-							}
-							$menuCabecera = array (
-									array (
-											"link" => "articulo/modal/nota-aclarativa/articulo/$articulo->id",
-											"descripcion" => "Añadir aclaración en la descripción",
-											"class" => "nmodal" 
-									),
-									array (
-											"link" => "product/nuevo/$articulo->id",
-											"descripcion" => "Poner en venta un	artículo similar",
-											"class" => "" 
-									),
-									array (
-											"link" => "articulo/modal/finalizar-anuncio/ofertas/$articulo->id",
-											"descripcion" => "Finalizar el anuncio",
-											"class" => "nmodal" 
-									) 
-							);
-						} else {
-							$nvendidos = 0;
-							if ($articulo->tipo == "Cantidad") {
-								$nvendidos = $this->articulo->obtenerVendidosDeCantidad ( $articulo->id );
-							}
-							if ($nvendidos == 0) {
-								if ($this->myuser && $this->myuser->id == $articulo->usuario->id) {
-									$tituloCabecera = "Ha publicado el anuncio correctamente. Ahora puedes:";
-									$menuCabecera = array (
-											array (
-													"link" => "product/edit/$articulo->id",
-													"descripcion" => "Editar este anuncio",
-													"class" => "" 
-											),
-											array (
-													"link" => "product/nuevo/$articulo->id",
-													"descripcion" => "Poner en venta un	artículo similar",
-													"class" => "" 
-											),
-											array (
-													"link" => "articulo/modal/finalizar-anuncio/ofertas/$articulo->id",
-													"descripcion" => "Finalizar el anuncio",
-													"class" => "nmodal" 
-											) 
-									);
-								}
-							} else {
-								if ($transaccion) {
-									if ($articulo->usuario->id == $this->myuser->id) {
-										$tituloCabecera = "<span class='green'>¡Felicidades!</span> Has vendido <strong>" . ($transaccion->cantidad > 1 ? "$transaccion->cantidad unidades" : "$transaccion->cantidad unidad") . "</strong> de este árticulo al usuario <a href='store/{$transaccion->comprador->seudonimo}' title='Ir al perfil {$transaccion->comprador->seudonimo}'>{$transaccion->comprador->seudonimo}</a> por <strong>" . formato_moneda ( $articulo->precio * $transaccion->cantidad ) . " $us</strong>";
-										$menuCabecera = array (
-												array (
-														"link" => "store/{$usuario->seudonimo}/sell/1/detail",
-														"descripcion" => "Ir a mis artículos vendidos.",
-														"class" => "" 
-												),
-												array (
-														"link" => "home/modal/modificar-cantidad/articulo/$articulo->id",
-														"descripcion" => "Modificar cantidad disponible.",
-														"class" => "nmodal" 
-												),
-												array (
-														"link" => "articulo/modal/nota-aclarativa/articulo/$articulo->id",
-														"descripcion" => "Añadir aclaración en la descripción",
-														"class" => "nmodal" 
-												),
-												array (
-														"link" => "articulo/modal/finalizar-anuncio/ofertas/$articulo->id",
-														"descripcion" => "Finalizar el anuncio",
-														"class" => "nmodal" 
-												) 
-										);
-									} else if ($transaccion->comprador->id == $this->myuser->id) {
-										$tituloCabecera = "<span class='green'>¡Felicidades!</span> Has comprado <strong>" . ($transaccion->cantidad > 1 ? "$transaccion->cantidad unidades" : "$transaccion->cantidad unidad") . "</strong> de este árticulo por <strong>" . formato_moneda ( $articulo->precio * $transaccion->cantidad ) . " $us</strong>";
-										$menuCabecera = array (
-												array (
-														"link" => "store/{$usuario->seudonimo}/self",
-														"descripcion" => "Ir a mis artículos comprados.",
-														"class" => "" 
-												) 
-										);
-									}
-								} else {
-									$tituloCabecera = "Tu artículo tiene ventas. Ahora puedes:";
-									$menuCabecera = array (
-											array (
-													"link" => "store/{$usuario->seudonimo}/sell/1/detail",
-													"descripcion" => "Ir a mis artículos vendidos.",
-													"class" => "" 
-											),
-											array (
-													"link" => "home/modal/modificar-cantidad/articulo/$articulo->id",
-													"descripcion" => "Modificar cantidad disponible.",
-													"class" => "nmodal" 
-											),
-											array (
-													"link" => "articulo/modal/nota-aclarativa/articulo/$articulo->id",
-													"descripcion" => "Añadir aclaración en la descripción",
-													"class" => "nmodal" 
-											),
-											array (
-													"link" => "articulo/modal/finalizar-anuncio/ofertas/$articulo->id",
-													"descripcion" => "Finalizar el anuncio",
-													"class" => "nmodal" 
-											) 
-									);
-								}
-							}
-						}
-					} else {
-						$maxof = $this->configuracion->variables ( "maximoCantidad" );
-						$uo = $this->articulo->ultimaOferta ( $articulo->id, $usuario->id );
-						$nof = $maxof - $uo->cantidad;
-						$mo = $this->articulo->mayorOferta ( $articulo->id, $articulo->tipo !== "Fijo" );
-						if ($uo) {
-							if ($articulo->tipo == "Fijo") {
-								$ofertaError = "";
-								switch ($uo->estado) {
-									case "Rechazado" :
-										$ofertaError = "<p><span class='red' style='font-weight:bold;'>Lo sentimos</span>, tu oferta ha sido rechazada.</p>";
-										break;
-									case "Pendiente" :
-										$ofertaError = "<p>Tu oferta ha sido enviada al vendedor.</p>";
-										break;
-								}
-								$ofertaError .= "<p>Tu oferta ha sido " . formato_moneda ( $uo->monto ) . " $us.</p>";
-								if ($nof > 0) {
-									$ofertaError .= "<p>Puedes hacer $nof ofertas más.</p>";
-								} else {
-									$ofertaError .= "<p>No puedes hacer más ofertas.</p>";
-								}
-							} elseif ($mo) {
-								$ofertaError = "";
-								if ($uo->id == $mo->id) {
-									$ofertaError = "<span class='green' style='font-weight:bold;'>¡Felicidades!</span> eres el máximo pujador.</p>";
-									$ofertaError .= "<p>Tu puja máxima son " . formato_moneda ( $uo->monto ) . " $us.</p>";
-									$ofertaError .= "<p>Puedes aumentar tu puja máxima para evitar que te sobrepujen en el último momento.</p>";
-								} else {
-									$ofertaError = "<span class='red' style='font-weight:bold;'>¡Lo sentimos!</span> te han sobrepujado.</p>";
-									$ofertaError .= "<p>Tu puja máxima son " . formato_moneda ( $uo->monto ) . " $us.</p>";
-									$ofertaError .= "<p>Aumenta tu puja para poder ser el máximo pujador.</p>";
-								}
-							}
-						}
-						$tituloCabecera = isset ( $ofertaError ) ? $ofertaError : "";
-						$menuCabecera = array ();
-					}
-				} else if ($articulo->estado == "A la venta") {
-					if ($articulo->tipo !== "Cantidad") {
-						if ($articulo->usuario->id == $this->myuser->id) {
-							$tituloCabecera = "Tu artículo ha finalizado sin venderse.";
-							$menuCabecera = array (
-									array (
-											"link" => "product/begin/$articulo->id",
-											"descripcion" => "Volver a poner en venta.",
-											"class" => "" 
-									),
-									array (
-											"link" => "store/{$usuario->seudonimo}/sell/3/detail",
-											"descripcion" => "Ir a mis artículos no vendidos.",
-											"class" => "" 
-									) 
-							);
-						} else {
-							$tituloCabecera = "<span class='red'>¡Lo sentimos!</span> El vendedor finalizó el anuncio anticipadamente.";
-							$menuCabecera = array ();
-						}
-					} else {
-						if ($transaccion) {
-							if ($articulo->usuario->id == $this->myuser->id) {
-								$tituloCabecera = "<span class='green'>¡Felicidades!</span> Has vendido <strong>" . ($transaccion->cantidad > 1 ? "$transaccion->cantidad unidades" : "$transaccion->cantidad unidad") . "</strong> de este árticulo al usuario <a href='store/{$transaccion->comprador->seudonimo}' title='Ir al perfil {$transaccion->comprador->seudonimo}'>{$transaccion->comprador->seudonimo}</a> por <strong>" . formato_moneda ( $articulo->precio * $transaccion->cantidad ) . " $us</strong>";
-								$menuCabecera = array (
-										array (
-												"link" => "store/{$usuario->seudonimo}/sell/1/detail",
-												"descripcion" => "Ir a mis artículos vendidos.",
-												"class" => "" 
-										),
-										array (
-												"link" => "home/modal/modificar-cantidad/articulo/$articulo->id",
-												"descripcion" => "Modificar cantidad disponible.",
-												"class" => "nmodal" 
-										),
-										array (
-												"link" => "articulo/modal/nota-aclarativa/articulo/$articulo->id",
-												"descripcion" => "Añadir aclaración en la descripción",
-												"class" => "nmodal" 
-										),
-										array (
-												"link" => "articulo/modal/finalizar-anuncio/ofertas/$articulo->id",
-												"descripcion" => "Finalizar el anuncio",
-												"class" => "nmodal" 
-										) 
-								);
-							} else if ($transaccion->comprador->id == $this->myuser->id) {
-								$tituloCabecera = "<span class='green'>¡Felicidades!</span> Has comprado <strong>" . ($transaccion->cantidad > 1 ? "$transaccion->cantidad unidades" : "$transaccion->cantidad unidad") . "</strong> de este árticulo por <strong>" . formato_moneda ( $articulo->precio * $transaccion->cantidad ) . " $us</strong>";
-								$menuCabecera = array (
-										array (
-												"link" => "store/{$usuario->seudonimo}/self",
-												"descripcion" => "Ir a mis artículos comprados.",
-												"class" => "" 
-										) 
-								);
-							}
-						} elseif ($articulo->usuario->id == $this->myuser->id) {
-							if ($articulo->cantidad == 0) {
-								$tituloCabecera = "Tu artículo ha quedado sin stock.";
-							} else {
-								$tituloCabecera = "Tu artículo ha finalizado.";
-							}
-							$linkx = "product/begin/$articulo->id";
-							$clasx = "";
-							if ($articulo->tipo == "Cantidad") {
-								if ($this->articulo->cantidad == 0) {
-									$linkx = "home/modal/modificar-cantidad/articulo/$articulo->id";
-									$clasx = "nmodal";
-								}
-							}
-							
-							$menuCabecera = array (
-									array (
-											"link" => $linkx,
-											"descripcion" => "Volver a poner en venta.",
-											"class" => $clasx 
-									),
-									array (
-											"link" => "product/nuevo/$articulo->id",
-											"descripcion" => "Poner en venta un	artículo similar",
-											"class" => "" 
-									) 
-							);
-						} else {
-							$tituloCabecera = "<span class='red'>¡Lo sentimos!</span> El vendedor finalizó el anuncio anticipadamente.";
-							$menuCabecera = array ();
-						}
-					}
-				} elseif ($articulo->comprador == $usuario->id) {
-					$tituloCabecera = "<span class='green' style='font-weight:bold;'>¡Felicidades!</span> ha comprado este artículo</p>";
-					$tituloCabecera .= "<p>Ahora el vendedor debe especificar los gastos de envío, podrás consultarlo en tu lista de <a href='store/{$usuario->seudonimo}/self'>articulos comprados</a>";
-					$menuCabecera = array ();
-				} elseif ($articulo->usuario->id !== $usuario->id) {
-					$tituloCabecera = "<span class='red' style='font-weight:bold;'>¡Lo sentimos!</span>, este artículo ha sido vendido a otro usuario.";
-					$menuCabecera = array ();
-				} elseif ($articulo->comprador) {
-					$og = $this->articulo->darOfertaGanadora ( $articulo->id );
-					if (! $og) {
-						$monto = $articulo->precio;
-					} else {
-						if ($articulo->tipo == "Fijo") {
-							$monto = $og->monto;
-						} else {
-							$monto = $og->monto_automatico;
-						}
-					}
-					$tituloCabecera = "<span class='green' style='font-weight:bold;'>!Felicidades¡</span> Tu artículo se ha vendido por " . formato_moneda ( $monto ) . " $us.";
-					$menuCabecera = array (
-							array (
-									"link" => "store/{$usuario->seudonimo}/sell",
-									"descripcion" => "Ir a mis artículos vendidos.",
-									"class" => "" 
-							),
-							array (
-									"link" => "product/nuevo/$articulo->id",
-									"descripcion" => "Poner en venta un	artículo similar",
-									"class" => "" 
-							) 
-					);
-				}
-				if (isset ( $menuCabecera ) && isset ( $tituloCabecera )) {
-					$this->load->view ( "usuario/menu-cabecera-perfil", array (
-							"titulo" => $tituloCabecera,
-							"listaMenu" => $menuCabecera 
-					) );
-				}
-				break;
+			break;
 		}
 		?></section>
 	</div><?php
