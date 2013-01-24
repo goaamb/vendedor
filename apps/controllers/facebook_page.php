53a27f183a0f48b3a8e9edc2b6ee16d3ac6c0236
Alvaro Justo Michel Barrera <goaamb@gmail.com>
2013-01-24 08:32:39 -0400

    - integracion con facebook

--- apps/controllers/facebook_page.php
+++ apps/controllers/facebook_page.php
@@ -4,6 +4,12 @@ require_once ('articulo.php');
 class Facebook_page extends Articulo {
 	public function __construct() {
 		parent::__construct ();
+		$this->load->library ( "facebook" );
+		$usuario = $this->mysession->userdata ( "facebook" );
+		if (! $usuario) {
+			$this->mysession->set_userdata ( "facebook", $this->facebook->getUser () );
+		}
+		var_dump($usuario);
 	}
 	public function index() {
 		$this->preheader = array_merge ( $this->preheader, array (
---
+++ apps/libraries/Facebook.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once ('apps/extra/facebook.php');
+class Facebook extends Real_Facebook {
+	public function __construct() {
+		Facebook::$CURL_OPTS [CURLOPT_CAINFO] = pathinfo ( __FILE__, PATHINFO_DIRNAME ) . '/../extra/fb_ca_chain_bundle.crt';
+		// Facebook::$CURL_OPTS[CURLOPT_SSL_VERIFYPEER] = true;
+		parent::__construct ( array (
+				'appId' => '108831492599921',
+				'secret' => 'adab39978c11f83f78a3f10274dbbd9c',
+				"cookie" => true 
+		) );
+	}
+}
+
+?>
