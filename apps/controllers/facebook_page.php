34d87eb93f5425946e774bb4e692dccb90d349a4
Alvaro Justo Michel Barrera <goaamb@gmail.com>
2013-01-24 09:19:34 -0400

    - integracion facebook

--- apps/controllers/facebook_page.php
+++ apps/controllers/facebook_page.php
@@ -9,13 +9,51 @@ class Facebook_page extends Articulo {
 		if (! $usuario) {
 			$this->mysession->set_userdata ( "facebook", $this->facebook->getUser () );
 		}
-		var_dump($usuario);
 	}
 	public function index() {
 		$this->preheader = array_merge ( $this->preheader, array (
-				"isFacebook" => true 
+				"isFacebook" => $this->mysession->userdata ( "facebook" ) 
 		) );
-		parent::nuevo ();
+		if ($this->iLike ()) {
+			parent::nuevo ();
+		}
+	}
+	private function checkPermission() {
+		$permit = true;
+		try {
+			$usuario = $this->mysession->userdata ( "facebook" );
+			$permissions = $this->facebook->api ( "/$usuario/permissions" );
+			if (is_array ( $permissions ) && count ( $permissions ) > 0 && isset ( $permissions ['data'] ) && is_array ( $permissions ['data'] ) && count ( $permissions ['data'] ) > 0) {
+				$myAppPermit = array (
+						'user_about_me',
+						'user_birthday',
+						'user_photos',
+						'publish_actions',
+						'email',
+						'publish_stream',
+						'share_item' 
+				);
+				foreach ( $myAppPermit as $mp ) {
+					if (! array_key_exists ( 'publish_actions', $permissions ['data'] [0] )) {
+						$permit = false;
+						continue;
+					}
+				}
+			} else {
+				return false;
+			}
+		} catch ( Exception $ex ) {
+			$permit = false;
+		}
+		return $permit;
+	}
+	private function iLike() {
+		$signed_request = $this->facebook->getSignedRequest ();
+		if (isset ( $signed_request ["page"] )) {
+			$like_status = $signed_request ["page"] ["liked"];
+			return $like_status;
+		}
+		return false;
 	}
 }
 
